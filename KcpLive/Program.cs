0691bf69b7ecdf4d7cfc2262d1429442cd4c531d
伟健 黄 <ejihuang@outlook.com>
2024-10-18 17:45:56 +0800

    update：增加录制屏幕功能，但是录制成mp4失败

--- KcpLive/Program.cs
+++ KcpLive/Program.cs
@@ -15,31 +15,64 @@ internal class Program
     private static CaptureZone<ColorBGRA> _fullscreen;
     private static CaptureZone<ColorBGRA> _topLeft;
 
+    private static SwScaler _scaler;
+    private static VideoEncoder _videoEncoder;
+    private static MediaMuxer _mediaMuxer;
+    private static MediaStream _videoStream;
+    private static Rational _frameRate = new(24, 1);
+
+    private static bool _isRecording = false;
+
     static void Main(string[] args)
     {
+        ffmpeg.RootPath = AppContext.BaseDirectory + "runtimes/win-x64/native";
+        ffmpeg.av_log_set_level(ffmpeg.AV_LOG_DEBUG);
+
         _screenCaptureService = new DX11ScreenCaptureService();
         _graphicsCards = _screenCaptureService.GetGraphicsCards();
         _displays = _screenCaptureService.GetDisplays(_graphicsCards.First());
         _screenCapture = _screenCaptureService.GetScreenCapture(_displays.First());
-        _screenCapture.Updated += OnScreenCaptureUpdated;
 
-        _fullscreen = _screenCapture.RegisterCaptureZone(0, 0, _screenCapture.Display.Width, _screenCapture.Display.Height);
+        _fullscreen = _screenCapture.RegisterCaptureZone(
+            0,
+            0,
+            _screenCapture.Display.Width,
+            _screenCapture.Display.Height
+        );
         _topLeft = _screenCapture.RegisterCaptureZone(0, 0, 100, 100, downscaleLevel: 1);
 
-        _screenCapture.CaptureScreen();
+        Console.WriteLine("[1] Capture and record.");
+        Console.WriteLine("[2] Capture and send.");
+
+        start:
+        Console.Write("Select number: ");
+
+        if (Console.ReadLine() == "1")
+        {
+            DoCaptureAndRecord();
+        }
+        else
+        {
+            goto start;
+        }
 
         Console.ReadKey();
     }
 
-    private static void OnScreenCaptureUpdated(object? sender, ScreenCaptureUpdatedEventArgs e)
+    private static void DoCaptureAndRecord()
     {
-        if (e.IsSuccessful)
+        Console.WriteLine("Screen capturing and recording...");
+        Console.WriteLine("Press ESC to stop.");
+        var frameNo = 0;
+
+        while (true)
         {
-            using (_topLeft.Lock())
+            _screenCapture.CaptureScreen();
+            using (_fullscreen.Lock())
             {
                 unsafe
                 {
-                    RefImage<ColorBGRA> image = _topLeft.RefImage;
+                    RefImage<ColorBGRA> image = _fullscreen.RefImage;
                     fixed (byte* ptr = &image.GetPinnableReference())
                     {
                         AVFrame* frame = ffmpeg.av_frame_alloc();
@@ -48,6 +81,66 @@ internal class Program
                         frame->format = (int)AVPixelFormat.AV_PIX_FMT_BGRA;
                         frame->data[0] = ptr;
                         frame->linesize[0] = image.RawStride;
+                        using var rgbFrame = new VideoFrame(frame, true);
+                        using var outFrame = new VideoFrame(
+                            image.Width,
+                            image.Height,
+                            PixelFormats.YUV420P
+                        );
+
+                        _scaler ??= new SwScaler(rgbFrame.Format, outFrame.Format);
+                        _scaler.Convert(rgbFrame, outFrame);
+
+                        if (_videoEncoder == null)
+                        {
+                            _videoEncoder = new VideoEncoder(
+                                MediaCodec.GetEncoder("libx264"),
+                                outFrame.Format,
+                                _frameRate
+                            );
+                            _videoEncoder.SetOption("crf", "24");
+                            _videoEncoder.SetOption("preset", "faster");
+                        }
+
+                        if (_mediaMuxer == null)
+                        {
+                            var fileName = "_cap.mp4";
+                            var muxerOpts = new List<KeyValuePair<string, string>>();
+                            if (fileName.EndsWith(".mp4"))
+                            {
+                                muxerOpts.Add(new("movflags", "+faststart"));
+                            }
+
+                            _mediaMuxer = new MediaMuxer(fileName);
+                            _videoStream = _mediaMuxer.AddStream(_videoEncoder);
+                            _mediaMuxer.Open(muxerOpts);
+
+                            //outFrame.Save("_demo.jpg");
+                        }
+
+                        outFrame.PresentationTimestamp = _videoEncoder.GetFramePts(frameNo);
+                        _mediaMuxer.EncodeAndWrite(_videoStream, _videoEncoder, outFrame);
+
+                        frameNo++;
+
+                        // 只录制10秒
+                        //if (frameNo > 240)
+                        //{
+                        //    _mediaMuxer.EncodeAndWrite(_videoStream, _videoEncoder, null);
+                        //    Console.WriteLine("Record finish.");
+                        //    break;
+                        //}
+
+                        if (Console.KeyAvailable)
+                        {
+                            var key = Console.ReadKey(intercept: true);
+                            if (key.Key == ConsoleKey.Escape)
+                            {
+                                _mediaMuxer.EncodeAndWrite(_videoStream, _videoEncoder, null);
+                                Console.WriteLine("Record finish.");
+                                break;
+                            }
+                        }
                     }
                 }
             }
